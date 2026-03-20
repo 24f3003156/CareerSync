a4b95aa18cbab42c18b7312adb5e14166fc2a3cc
Aakriti <24f3003156@ds.study.iitm.ac.in>
2026-03-20 13:25:55 +0530

    Company model CReated

--- models.py
+++ models.py
@@ -9,3 +9,21 @@ class Admin(db.Model):
     pass_hash = db.Column(db.string(100), nullable = False )
     f_name = db.Column(db.string(100), nullable = False)
     l_name = db.Column(db.string(100))
+
+class Company(db.Model):
+    id = db.Column(db.integer, primary_key = True)
+    company_name = db.Column(db.string(100), nullable = False)
+    email = db.Column(db.string, nullable = False, unique = True)
+    pass_hash = db.Column(db.string(100), nullable = False )
+    hr_contact = db.Column(db.integer, nullable = False)
+    website = db.Column(db.string(100), nullable = False)
+    domain = db.Column(db.string(100), nullable = False)
+    description = db.Column(db.text, nullable = False)
+    approval_status = db.Column(db.string(50), default = "Pending")
+    is_active = db.Column(db.boolean, default = True)
+    created_at = db.Column(db.datetime, default = datetime.utcnow)
+    type_of_employment = db.Column(db.string(100))
+
+
+
+
