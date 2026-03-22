0f3d7931d13136e6f99a444fab2326db085708e7
Aakriti <24f3003156@ds.study.iitm.ac.in>
2026-03-22 18:05:36 +0530

    Work in progress

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
-from models import db
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from werkzeug.security import generate_password_hash, check_password_hash
+from models import db, Admin, Company, Student
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///placement_portal.db"
@@ -7,3 +8,28 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "secret_key"
 
 db.init_app(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+    if request.method == "POST":
+        role = request.form.get("role")
+        email_or_username = request.form.get("email_or_username")
+        password = request.form.get("password")
+
+        if role == "admin":
+            admin = Admin.query.filter_by(username = email_or_username).first()
+
+            if admin and check_password_hash(admin.pass_hash, password):
+                session["user_id"] = admin.id
+                session["role"] = "admin"
+                flash("Admin login successful.")
+                return(redirect(url_for("index")))
+            else:
+                flash("Invalid admin credentials.")
+
+if __name__ == "__main__":
+    app.run(debug= True)
