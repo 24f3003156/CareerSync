017d3bd68594e422faf04662ebc4cc6a87291705
Aakriti <24f3003156@ds.study.iitm.ac.in>
2026-03-23 21:52:02 +0530

    authentication logic updated

--- app.py
+++ app.py
@@ -9,6 +9,16 @@ app.config["SECRET_KEY"] = "secret_key"
 
 db.init_app(app)
 
+def admin_logged_in():
+    return "user_id" in session and session.get("role") =="admin"
+
+def company_logged_in():
+    return "user_id" in session and session.get("role") =="company"
+
+def student_logged_in():
+    return "user_id" in session and session.get("role") =="student"
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -26,27 +36,39 @@ def login():
                 session["user_id"] = admin.id
                 session["role"] = "admin"
                 flash("Admin login successful.")
-                return(redirect(url_for("index")))
+                return(redirect(url_for("admin_dashboard")))
             else:
                 flash("Invalid admin credentials.")
 
         elif role == "company":
             company = Company.query.filter_by(email = email_or_username).first()
             if company and check_password_hash(company.pass_hash, password):
+
+                if company.approval_status != "Approved":
+                    flash("Company not yet approved by admin.")
+                    return redirect(url_for("login"))
+                
+                if company.is_active == False:
+                    flash("Company account is deactivated.")
+                    return redirect(url_for("login"))
+                
                 session["user_id"] = company.id
                 session["role"] = "company"
                 flash("Company login successfull.")
-                return redirect(url_for("index"))
+                return redirect(url_for("company_dashboard"))
             else:
                 flash("Invalid company credentials.")
 
         elif role == "student":
             student = Student.query.filter_by(email = email_or_username).first()
             if student and check_password_hash(student.pass_hash, password):
+                if student.is_active == False:
+                    flash("Student account is deactivated.")
+                    return redirect(url_for("login"))
                 session["user_id"] = student.id
                 session["role"] = "student"
                 flash("Student login successfull.")
-                return redirect(url_for("index"))
+                return redirect(url_for("student_dashboard"))
             else:
                  flash("Invalid student credentials.")
 
@@ -138,6 +160,35 @@ def register_student():
     
     return render_template("register_student.html")
 
+@app.route("/admin/dashboard")
+def admin_dashboard():
+    if not admin_logged_in():
+        flash("Unauthorized access.")
+        return redirect(url_for("login"))
+    return render_template("admin_dashboard.html")
+
+@app.route("/company/dashboard")
+def company_dashboard():
+    if not company_logged_in():
+        flash("Unauthorized access.")
+        return redirect(url_for("login"))
+    return render_template("company_dashboard.html")
+
+@app.route("/student/dashboard")
+def student_dashboard():
+    if not student_logged_in():
+        flash("Unauthorized access.")
+        return redirect(url_for("login"))
+    return render_template("student_dashboard.html")
+
 
 if __name__ == "__main__":
     app.run(debug= True)
+
+def admin_logged_in():
+    return "user_id" in session and session.get("role") == "admin"
+
+def student_logged_in():
+    return "user_id" in session and session.get("role") == "student"
+
+    
